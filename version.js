cba62f215825a2469e2748479eae62752c039781
MetalfOxXer <66499312+MetalfOxXer@users.noreply.github.com>
2021-07-28 17:47:01 +0200

    Update version.js

--- version.js
+++ version.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     version: '7.7',
-    download_link: 'https://drive.google.com/file/d/120wvFYEaYiHJAEKD6tzPPhBoc0BigQV6/view?usp=sharing',
+    download_link: 'https://drive.google.com/file/d/1I7pPluY9D4hIJ7uTAqjV7DTY_bN84j60/view?usp=sharing',
     changelog: 'http://www.narutoninpou.com/#/forum/threads/602aa9d385a02400177afdc8'
 }
