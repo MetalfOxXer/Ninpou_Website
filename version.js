fe784bc1f4b2a0d84a13c6d2e4db7741adecb2b4
MetalfOxXer <66499312+MetalfOxXer@users.noreply.github.com>
2021-08-28 11:31:45 +0200

    Update version.js

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    version: '8.0',
-    download_link: 'https://drive.google.com/file/d/15vJivsM3LREz3Cam6OP6g86zuGgAgAQT/view?usp=sharing',
+    version: '8.1',
+    download_link: 'https://drive.google.com/file/d/1zcz4YgFVRXtcHZZmQd_Qa4MSjuZwW25k/view?usp=sharing',
     changelog: 'http://www.narutoninpou.com/#/forum/threads/602aa9d385a02400177afdc8'
 }
